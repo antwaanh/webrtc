250c672f054d95859bd2d844ce9f9376a20d6dc2
Antwaan Harrison <antwaanvh@gmail.com>
2018-04-28 16:51:45 -0400

    Add mix and react

---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+import React from 'react'
+import ReactDom from 'react-dom'
+
+ReactDom.render(
+  <div>
+    <div className='logo'><p /></div>
+    <div className='title'><p /></div>
+    <div className='subtitle'>
+      <p>AdonisJs + React will make you feel confident about your code</p>
+    </div>
+  </div>,
+  document.getElementById('root')
+)
